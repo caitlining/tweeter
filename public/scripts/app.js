8809ca62cd3e15fc8fc2bd1673bdf17a842ac861
caitlining <caitlincing@gmail.com>
2019-10-09 21:19:10 +0000

    Add loadTweets function which uses jQuery to make a GET request to /tweets and then renders those tweets to the page by using our previously made renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,42 +6,6 @@
 
 $(document).ready(function() {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants. "
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1570391390081
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Caitlin",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@me_ing" },
-  //     "content": {
-  //       "text": "I am testing an additional object!"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
-
   const createTweetElement = function(tweetObj) {
     const $tweet = $("<article>").addClass("tweet");
     const daysAgo = daysSinceTweet(tweetObj["created_at"])
@@ -81,8 +45,14 @@ $(document).ready(function() {
     }
   };
 
-  // renderTweets(data);
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (allTweets) {
+      renderTweets(allTweets);
+    })
+  }
 
+  loadTweets();
 
   $('.new-tweet form').submit( function (event) {
     event.preventDefault();

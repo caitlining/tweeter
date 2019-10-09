c9406932713d8fed786c08021207a8117da328fa
caitlining <caitlincing@gmail.com>
2019-10-09 21:03:53 +0000

    Add AJAX POST request on new-tweet form submission to post the new tweet text to /tweets/

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,40 +6,40 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants. "
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1570391390081
-    },
-    {
-      "user": {
-        "name": "Caitlin",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@me_ing" },
-      "content": {
-        "text": "I am testing an additional object!"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants. "
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1570391390081
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Caitlin",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@me_ing" },
+  //     "content": {
+  //       "text": "I am testing an additional object!"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
 
   const createTweetElement = function(tweetObj) {
@@ -81,7 +81,13 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
 
+  $('.new-tweet form').submit( function (event) {
+    event.preventDefault();
+    const $form = $(this);
+    const tweet = $form.serialize()
+    $.ajax({ url: "/tweets/", method: 'POST', data: tweet })
+  })
 });

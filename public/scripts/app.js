d5be49c1074432756f3087c2d7cb165ed44b4bc1
caitlining <caitlincing@gmail.com>
2019-10-09 03:32:45 +0000

    Add daysSinceTweet function to return the number of days since the tweet has been made, implement this function within createTweetElement so that the corrent representation of time is being appended to our Tweeter page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,44 +6,45 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Caitlin",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@me_ing" },
-      "content": {
-        "text": "I am testing an additional object!"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1570391390081
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Caitlin",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@me_ing" },
+  //     "content": {
+  //       "text": "I am testing an additional object!"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
 
   const createTweetElement = function(tweetObj) {
     const $tweet = $("<article>").addClass("tweet");
+    const daysAgo = daysSinceTweet(tweetObj["created_at"])
 
     const innerHTML = `
           <header>
@@ -53,7 +54,7 @@ $(document).ready(function() {
           </header>
           <p>${tweetObj.content.text}</p>
           <footer>
-            <p>${tweetObj["created_at"]}</p>
+            <p>${daysAgo} days ago</p>
             <h4>PIN RETWEET HEART</h4>
           </footer>
           `;
@@ -62,6 +63,17 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  const daysSinceTweet = function(epochOfTweet) {
+    const currentDate = new Date();
+    const currentTime = currentDate.getTime();
+    const millisecondsInDay = 86400000;
+
+    const timeDifference = currentTime - epochOfTweet;
+    const dayDifference = timeDifference/millisecondsInDay;
+
+    return Math.floor(dayDifference);
+  }
+
   const renderTweets = function(tweetObjArr) {
     for (const tweet of tweetObjArr) {
       const $tweet = createTweetElement(tweet);
@@ -69,7 +81,7 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
 
 });

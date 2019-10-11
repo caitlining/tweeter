dcd3132f2b66eea1f175a468ba66b8793f245e16
caitlining <caitlincing@gmail.com>
2019-10-11 00:09:27 +0000

    Refactoring to add missing semicolon

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
-  // Compass says the event should be change, keydown, keyup, blur, or keypress
-  //May need to come back and change before submission
-
+ /**
+ * Responds to input on textarea with the class new-tweet
+ * Checks the length of the input to see if the 'tweetTooLong' styling class needs to be added or removed
+ * Changes the text of the sibling with 'counter' class to be the current input length
+ */
   $('.new-tweet textarea').on('input', function() {
     let newTweetLength = $(this).val().length;
     let nearbyCounter = $(this).siblings('.counter');
-    const tweetLengthLimit = 140
+    const tweetLengthLimit = 140;
 
     if (newTweetLength > tweetLengthLimit) {
       nearbyCounter.addClass('tweetTooLong');

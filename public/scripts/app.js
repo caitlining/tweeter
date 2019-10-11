31944ef940d8a5b76fa11b7ffde5b76d396806e5
caitlining <caitlincing@gmail.com>
2019-10-11 00:29:53 +0000

    Refactoring to add comments and correct style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,24 +6,24 @@
 
 $(document).ready(function() {
 
-/**
- * Takes in a string
- * Prevents XSS by converting the string to a text node
- * Returns the inner HTML of the created text node 
- */
+  /**
+   * Takes in a string
+   * Prevents XSS by converting the string to a text node
+   * Returns the inner HTML of the created text node
+   */
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
   /**
    * Takes in a new tweet object
-   * Returns a new html article, with all tweet information properly organized
+   * Returns a new html article, with all tweet information organized into correct elements
    */
   const createTweetElement = function(tweetObj) {
     const $tweet = $("<article>").addClass("tweet");
-    const daysAgo = daysSinceTweet(tweetObj["created_at"])
+    const daysAgo = daysSinceTweet(tweetObj["created_at"]);
 
     const innerHTML = `
           <header>
@@ -42,17 +42,26 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  /**
+   * Takes a date in epoch time
+   * Returns the whole number of days difference between the input time and current date
+   */
   const daysSinceTweet = function(epochOfTweet) {
     const currentDate = new Date();
     const currentTime = currentDate.getTime();
     const millisecondsInDay = 86400000;
 
     const timeDifference = currentTime - epochOfTweet;
-    const dayDifference = timeDifference/millisecondsInDay;
+    const dayDifference = timeDifference / millisecondsInDay;
 
     return Math.floor(dayDifference);
-  }
+  };
 
+  /**
+   * Takes an array of tweet objects
+   * Runs each tweet object through our createTweetElement function
+   * Prepends each returned tweet element to the html section with class 'all-tweets'
+   */
   const renderTweets = function(tweetObjArr) {
     for (const tweet of tweetObjArr) {
       const $tweet = createTweetElement(tweet);
@@ -60,17 +69,29 @@ $(document).ready(function() {
     }
   };
 
-  const loadTweets = function () {
-    $.ajax('/tweets', { method: 'GET' })
-    .then(function (allTweets) {
-      renderTweets(allTweets);
-    })
-  }
+  /**
+   * Makes a GET request to the tweet database
+   * Runs the returned tweet array through our renderTweets function
+   */
+  const loadTweets = function() {
+    $.ajax('/tweets/', { method: 'GET' })
+      .then(function(allTweets) {
+        renderTweets(allTweets);
+      });
+  };
 
   loadTweets();
 
-  $('.new-tweet form').submit( function (event) {
-    event.preventDefault();   
+  /**
+   * Triggered on submission of the form with the class 'new-tweet'
+   * Empties and slides up any existing error messages
+   * Checks the length of the text being submitted and runs error messages if necessary
+   * If no errors from text length, makes POST request with form text to /tweets/
+   * Then makes a GET request to /tweets/
+   * Then resets the form and uses our renderTweets function to add the new tweet to the page 
+   */
+  $('.new-tweet form').submit(function(event) {
+    event.preventDefault();
     $('.new-tweet p').empty().slideUp();
     const $form = $(this);
     const newTweetTextStr = $form.children('textarea').val();
@@ -80,7 +101,7 @@ $(document).ready(function() {
       setTimeout(() => {
         $('.new-tweet p').slideDown();
       }, 600);
-    } else if (newTweetTextStr.length > 140) { 
+    } else if (newTweetTextStr.length > 140) {
       $('.new-tweet p').append("<b>Error:</b> We do not accept tweets longer than 140 characters. Your tweet is currently too long.");
       setTimeout(() => {
         $('.new-tweet p').slideDown();
@@ -88,24 +109,29 @@ $(document).ready(function() {
     } else {
       const tweet = $form.serialize();
       $.ajax({ url: "/tweets/", method: 'POST', data: tweet })
-      .then (function (successfulPost) {
-        return $.ajax('/tweets', { method: 'GET' })
+      .then (function(successfulPost) {
+        return $.ajax('/tweets/', { method: 'GET' })
       })
-      .then (function (allTweetsArr) {
+      .then (function(allTweetsArr) {
         $form[0].reset();
         $form.children('span').text(140);
         const latestTweet = [allTweetsArr[allTweetsArr.length - 1]];
         renderTweets(latestTweet);
       })
-      .fail(function (err) {
+      .fail(function(err) {
         alert(err.responseJSON.error);
       })
     }
   })
 
-  $('#compose-new button').click(function () {
+  /**
+   * Triggered on click of the button with id 'compose-new'
+   * Toggles the html section with the class 'new-tweet'
+   * Focuses on the text area of the html section with the class 'new-tweet'
+   */
+  $('#compose-new button').click(function() {
     $('section.new-tweet').slideToggle("slow");
     $('section.new-tweet textarea').focus();
-  })
+  });
 
 });
